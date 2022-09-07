3eb6f30d72561c3b45c2dc64447368bd8b3725a6
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2022-09-07 11:46:55 +0400

    Create basic game variables

--- modules/game.js
+++ modules/game.js
@@ -1,5 +1,44 @@
 export const GameBoard = (function () {
-  // assignCell(playerSign)
-  // checkIfCellIsEmpty -> assignCell(sign);
+  const roundCount = 1;
+  const areAllCellsAssigned = false;
+  const cells = [];
+  // by for loops -> name the cell and cell.isEmpty assign;
+  // cell1.isEmpty = true;
+  // cells.push(cell);
   //
+  // checkIfAllCellsAreAssigned () {
+  //  return cells.some((cell)=> (cell.isEmpty === true) )
+  // }
+  // pickCell(pickedCell, player) {
+  //  if(!pickedCell.isEmpty)
+  //  -> pickedCell.isEmpty = false;
+  //  -> assignCell(pickedCell, player.sign);
+  // }
+  // assignCell(pickedCell, player.sign) {
+  //   pickedCell.assignedPlayer = player.sign;
+  //  roundCount++;
+  //  if(roundCount === 9) checkWinningLine()
+  // }
+  //
+  // checkWinningLine() {
+  //    const winningCases = [
+  //      [1,2,3],[4,5,6,],[7,8,9],
+  //      [1,4,7],[2,5,8],[3,6,9],
+  //       [1,5,9],[3,5,7]
+  //     ]
+  //  for(let case in winningCases){
+  // if(case[0].assignedPlayer == case[1].assignedPlayer == case[2].assignedPlayer) {
+  //     const winnerSign = case[2].assignedPlayer;
+  //     return winnerSign
+  //        }
+  //    } else return "draw"
+  // }
+  // announceResults() {
+  //  const results = checkingWinningLine();
+  //  if(results == x)
+  //  elseif(results == 0)
+  //  else(results == draw)
+  // }
+
+  // return {}
 })();
