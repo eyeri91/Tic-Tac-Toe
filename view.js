4e9ad58bca91c3041662d7237a522400492c0b7e
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2022-09-12 13:03:07 +0400

    Add a function to render start page that creates basic HTML elements

--- view.js
+++ view.js
@@ -7,7 +7,27 @@ export const Display = (function (appContainer) {
     return element;
   }
 
-  //   function renderStartPage() {}
+  function renderStartPage() {
+    const gameTitle = createElement("h1", "Tic-Tac-Toe");
+    appContainer.append(gameTitle);
+
+    const selectPlayerParagraph = createElement("p", "Select the player!");
+    appContainer.append(selectPlayerParagraph);
+
+    const playerButtonsContainer = createElement("div");
+    appContainer.append(playerButtonsContainer);
+
+    const player1Button = createElement("button", "X");
+    playerButtonsContainer.append(player1Button);
+
+    const player2Button = createElement("button", "O");
+    playerButtonsContainer.append(player2Button);
+
+    const startGameButton = createElement("button", "Start Game");
+    appContainer.append(startGameButton);
+  }
   //   renderGamePage()
   //renderResults()
+
+  return { renderStartPage };
 })(appContainer);
