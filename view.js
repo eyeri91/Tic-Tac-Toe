76a1700e192aa35866ac71068701e67f6ea65aba
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2022-09-12 13:33:52 +0400

    Add a function to create 9 cells and append them to gameBoardGrid container

--- view.js
+++ view.js
@@ -7,6 +7,15 @@ export const Display = (function (appContainer) {
     return element;
   }
 
+  function createGridCells(gridBoard) {
+    for (let i = 0; i < 9; i++) {
+      const cell = createElement("div");
+      cell.id = i;
+      cell.className.add("grid-item");
+      gridBoard.append(cell);
+    }
+  }
+
   function renderStartPage() {
     const gameTitle = createElement("h1", "Tic-Tac-Toe");
     appContainer.append(gameTitle);
@@ -26,8 +35,29 @@ export const Display = (function (appContainer) {
     const startGameButton = createElement("button", "Start Game");
     appContainer.append(startGameButton);
   }
-  //   renderGamePage()
-  //renderResults()
 
-  return { renderStartPage };
+  function renderGamePage() {
+    const resultDisplayContainer = createElement("div");
+    appContainer.append(resultDisplayContainer);
+
+    const mainGameDisplayContainer = createElement("div");
+    appContainer.append(mainGameDisplayContainer);
+
+    const player1Div = createElement("div", "X");
+    mainGameDisplayContainer.append(player1Div);
+
+    const gameBoardGrid = createElement("div");
+    mainGameDisplayContainer.append(gameBoardGrid);
+
+    createGridCells(gameBoardGrid);
+
+    const player2Div = createElement("div", "O");
+    mainGameDisplayContainer.append(player2Div);
+
+    const replayGameButtonContainer = createElement("div");
+    appContainer.append(replayGameButtonContainer);
+  }
+  //updateResults()
+
+  return { renderStartPage, renderGamePage };
 })(appContainer);
