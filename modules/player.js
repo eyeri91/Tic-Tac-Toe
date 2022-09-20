5b81e67670a73beb29c19af7727d38a3147c1abc
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2022-09-20 22:13:02 +0400

    Implemnet basic Player module

--- modules/player.js
+++ modules/player.js
@@ -1,13 +1,7 @@
-export const Player = function (name, sign, isCurrentlyPlaying) {
-  const player = {
-    name,
-    sign,
-    isCurrentlyPlaying,
-  };
-
+const Player = (name, sign, isCurrentlyPlaying) => {
   function togglePlayingStatus(player) {
     player.isCurrentlyPlaying = !player.isCurrentlyPlaying;
   }
 
-  return { player, togglePlayingStatus };
+  return { name, sign, isCurrentlyPlaying, togglePlayingStatus };
 };
