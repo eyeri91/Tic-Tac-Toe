5e44c9a0ed3383f9504fe61be66bffd13cef26f4
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2022-11-02 18:43:51 +0400

    Add src and dist folders

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { Controller } from "./modules/controller.js";
+import { Controller } from "../modules/controller.js";
 const gameApp = document.getElementById("gameContainer");
 
 const display = Controller(gameApp);
